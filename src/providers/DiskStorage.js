6fead61917855a42d8b27ba038e8c173222496ef
Raí Soares <raisoares.dev@gmail.com>
2023-07-26 02:52:17 -0300

    DiskStorage fix

--- src/providers/DiskStorage.js
+++ src/providers/DiskStorage.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 const uploadConfig = require("../configs/upload");
-const { promises } = require("dns");
 
 class DiskStorage {
     async saveFile(file) {
