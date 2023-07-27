fe75f1c799948dc25a6da1878a01ca8e506753fa
Raí Soares <raisoares.dev@gmail.com>
2023-07-27 01:38:34 -0300

    module cors added

--- src/server.js
+++ src/server.js
@@ -3,9 +3,10 @@ const AppError = require("./utils/AppError");
 const uploadConfig = require("./configs/upload");
 
 //cirando o servidor
+const cors = require("cors");
 const express = require("express");
 const {request, response, json} = require("express");
-const routes = require("./routes")
+const routes = require("./routes");
 
 const PORT = 3333;
 const app = express();
@@ -13,6 +14,7 @@ const app = express();
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 app.use(express.json());
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
+app.use(cors());
 app.use(routes);
 
 app.use((error, request, response, next) => {
