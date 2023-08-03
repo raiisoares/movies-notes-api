df952df204c66a8a48d017c710e4f4cb4728cd04
Raí Soares <raisoares.dev@gmail.com>
2023-08-03 03:00:06 -0300

    try to fix a bug on diskStorage

--- src/controllers/UserAvatarController.js
+++ src/controllers/UserAvatarController.js
@@ -1,6 +1,6 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
-const DiskStorage = require("../providers/diskStorage");
+const DiskStorage = require("../providers/DiskStorage");
 
 class UserAvatarController {
     async update(request, response) {
